668c0462e208f9dd1a5ae50eabcf86a0dc661ff1
CodeDarkJeff <jeffryha@outlook.com>
2024-11-13 16:14:22 -0600

    Cambios nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { initializeApp } from "firebase/app";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCYPojh0AazpUuUP9pXje7Ppl2cvxotFEM",
+  authDomain: "bodayurliss.firebaseapp.com",
+  projectId: "bodayurliss",
+  storageBucket: "bodayurliss.firebasestorage.app",
+  messagingSenderId: "1064151455569",
+  appId: "1:1064151455569:web:f8f95326b8816b856f2570"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [
     provideClientHydration()
