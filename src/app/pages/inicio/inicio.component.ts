e8b21561fce20bc838a8a9ce9b4e7583094f9edc
CodeDarkJeff <jeffryha@outlook.com>
2024-11-18 13:04:02 -0600

    Ajustes btn whatsapp

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
+  ubicacion(){
+    window.open('https://maps.app.goo.gl/qqBWHYTZpH7jZDKY7?g_st=com.google.maps.preview.copy');
+  }
+  confirmaNovio(){
+    window.open('https://wa.me/+50685328910?text=¡Hola!%20Confirmo%20mi%20asistencia.');
 
+  } confirmaNovia(){
+    window.open('https://wa.me/+50686641432?text=¡Hola!%20Confirmo%20mi%20asistencia.');
+
+  }
 }
